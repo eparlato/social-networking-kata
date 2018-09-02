ebdc5517f17c1fe65175d2054bf9e64ca1bed91b
eparlato <eparlato82@gmail.com>
2018-09-02 16:13:26 +0200

    Package e2e in test scope becomes acceptance.

--- src/test/java/it/eparlato/socialnetworking/acceptance/SocialNetworkingApplicationShould.java
+++ src/test/java/it/eparlato/socialnetworking/acceptance/SocialNetworkingApplicationShould.java
@@ -1,16 +1,16 @@
-package it.eparlato.socialnetworking.e2e;
+package it.eparlato.socialnetworking.acceptance;
 
 import it.eparlato.socialnetworking.SocialNetworkProcessor;
-import it.eparlato.socialnetworking.SocialNetworkingApp;
 import it.eparlato.socialnetworking.parser.ConcreteInputParser;
-import it.eparlato.socialnetworking.parser.DummyInputParser;
 import it.eparlato.socialnetworking.time.TweakedApplicationClock;
 import it.eparlato.socialnetworking.user.repository.InMemoryUserRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -18,7 +18,7 @@ public class SocialNetworkingApplicationShould {
     ByteArrayOutputStream canvas = new ByteArrayOutputStream();
     PrintStream originalSystemOut = System.out;
     PrintStream hijackedSystemOut = new PrintStream(canvas);
-    String commands;
+
 
     @Before
     public void setup() {
@@ -31,21 +31,6 @@ public class SocialNetworkingApplicationShould {
         // Restore the original system.out
         System.setOut(originalSystemOut);
     }
-
-    @Test
-    public void return_an_error_message_if_I_just_write_a_user_name() throws UnsupportedEncodingException {
-        commands = "Alice";
-
-        SocialNetworkingApp socialNetworkingApp = new SocialNetworkingApp(getInputStreamFrom(commands), System.out, new DummyInputParser());
-        socialNetworkingApp.run();
-
-        String expected = "User Alice doesn't exist" + System.getProperty("line.separator");
-
-        assertEquals(expected, canvas.toString("UTF-8"));
-    }
-
-    // TODO: this package is not going to be e2e anymore, but acceptance
-    // remove the first tests, the e2e one. It's useless
 
     @Test
     public void show_the_user_timeline_when_the_user_name_is_typed() throws UnsupportedEncodingException {
@@ -65,12 +50,8 @@ public class SocialNetworkingApplicationShould {
 
         String expected =
                 "Good game though. (1 minute ago)" + System.getProperty("line.separator") +
-                "Damn! We lost! (2 minutes ago)" + System.getProperty("line.separator");
+                        "Damn! We lost! (2 minutes ago)" + System.getProperty("line.separator");
 
         assertEquals(expected, canvas.toString("UTF-8"));
-    }
-
-    private InputStream getInputStreamFrom(String commands) throws UnsupportedEncodingException {
-        return new ByteArrayInputStream(commands.getBytes("UTF-8"));
     }
 }
