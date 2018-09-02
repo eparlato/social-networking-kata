ffc0d155b6e8722edd542207a81bc232fe1aa4dd
eparlato <eparlato82@gmail.com>
2018-09-02 09:59:29 +0200

    Refactoring.

--- src/test/java/it/eparlato/socialnetworking/SocialNetworkProcessorShould.java
+++ src/test/java/it/eparlato/socialnetworking/SocialNetworkProcessorShould.java
@@ -2,11 +2,8 @@ package it.eparlato.socialnetworking;
 
 import it.eparlato.socialnetworking.command.Command;
 import it.eparlato.socialnetworking.command.Read;
-import it.eparlato.socialnetworking.parser.ConcreteInputParser;
 import it.eparlato.socialnetworking.parser.InputParser;
 import it.eparlato.socialnetworking.command.Publish;
-import it.eparlato.socialnetworking.time.TweakedApplicationClock;
-import it.eparlato.socialnetworking.user.InMemoryUserRepository;
 import it.eparlato.socialnetworking.user.User;
 import it.eparlato.socialnetworking.user.UserRepository;
 import org.jmock.Expectations;
@@ -18,66 +15,61 @@ import java.util.LinkedList;
 
 
 public class SocialNetworkProcessorShould {
+    public static final int IRRELEVANT_TIME_OF_PUBLISHING = 0;
+
     @Rule
     public final JUnitRuleMockery context = new JUnitRuleMockery();
 
-    // TODO: refactor!
+    final InputParser parser = context.mock(InputParser.class);
+    final UserRepository userRepository = context.mock(UserRepository.class);
+    final User user = context.mock(User.class);
+    SocialNetworkProcessor socialNetworkProcessor = new SocialNetworkProcessor(parser);
 
     @Test
     public void execute_a_publish_command_when_the_input_is_a_publish() {
-        final String command = "Alice -> I love the weather today";
-        final InputParser parser = context.mock(InputParser.class);
-        final UserRepository userRepository = context.mock(UserRepository.class);
-        final User userAlice = context.mock(User.class);
-
-        SocialNetworkProcessor socialNetworkProcessor = new SocialNetworkProcessor(parser);
+        final String input = "Alice -> I love the weather today";
+        final String message = "I love the weather today";
 
         final Command post = new Publish("Alice",
-                "I love the weather today",
-                userRepository, 0);
+                message,
+                userRepository, IRRELEVANT_TIME_OF_PUBLISHING);
 
-        context.checking(new Expectations(){
+        context.checking(new Expectations() {
             {
-                oneOf(parser).parse(command);
+                oneOf(parser).parse(input);
                 will(returnValue(post));
 
                 oneOf(userRepository).getUser("Alice");
-                will(returnValue(userAlice));
+                will(returnValue(user));
 
-                oneOf(userAlice).post("I love the weather today");
+                oneOf(user).post(message);
             }
         });
 
-        socialNetworkProcessor.process("Alice -> I love the weather today");
+        socialNetworkProcessor.process(input);
     }
 
     @Test
     public void execute_a_read_command_if_the_input_is_a_reading() {
-
-        final InputParser parser = context.mock(InputParser.class);
-        final UserRepository userRepository = context.mock(UserRepository.class);
-        final User userCharlie = context.mock(User.class);
-
-        SocialNetworkProcessor socialNetworkProcessor = new SocialNetworkProcessor(parser);
+        final String input = "Charlie";
 
         final Command read = new Read("Charlie",
                 userRepository,
-                0);
-
+                IRRELEVANT_TIME_OF_PUBLISHING);
 
         context.checking(new Expectations() {
             {
-                oneOf(parser).parse("Charlie");
+                oneOf(parser).parse(input);
                 will(returnValue(read));
 
                 oneOf(userRepository).getUser("Charlie");
-                will(returnValue(userCharlie));
+                will(returnValue(user));
 
-                oneOf(userCharlie).read();
+                oneOf(user).read();
                 will(returnValue(new LinkedList<Message>()));
             }
         });
 
-        socialNetworkProcessor.process("Charlie");
+        socialNetworkProcessor.process(input);
     }
 }
