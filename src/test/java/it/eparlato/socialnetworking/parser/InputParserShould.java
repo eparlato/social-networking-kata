44a679710b031393438082f1d6085b49fda471ce
eparlato <eparlato82@gmail.com>
2018-09-02 10:13:43 +0200

    ConcreteInputParser can parse an input with a Read command.

--- src/test/java/it/eparlato/socialnetworking/parser/InputParserShould.java
+++ src/test/java/it/eparlato/socialnetworking/parser/InputParserShould.java
@@ -2,18 +2,38 @@ package it.eparlato.socialnetworking.parser;
 
 import it.eparlato.socialnetworking.command.Command;
 import it.eparlato.socialnetworking.command.Publish;
+import it.eparlato.socialnetworking.command.Read;
 import it.eparlato.socialnetworking.time.TweakedApplicationClock;
 import it.eparlato.socialnetworking.user.DummyUserRepository;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.time.MonthDay;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class InputParserShould {
+    Command expectedCommand;
+    InputParser parser;
+    String input;
+
+    @Before
+    public void setup() {
+        parser = new ConcreteInputParser(new DummyUserRepository(), new TweakedApplicationClock(System.currentTimeMillis()));
+    }
+
     @Test
     public void return_a_publish_command_if_the_input_is_publish_a_message() {
-        Command expectedCommand = new Publish("Bob", "Damn! We lost!", new DummyUserRepository(), 0);
-        InputParser parser = new ConcreteInputParser(new DummyUserRepository(), new TweakedApplicationClock(System.currentTimeMillis()));
-        String input = "Bob -> Damn! We lost!";
+        input = "Bob -> Damn! We lost!";
+        expectedCommand = new Publish("Bob", "Damn! We lost!", new DummyUserRepository(), 0);
+
+        assertEquals(expectedCommand, parser.parse(input));
+    }
+
+    @Test
+    public void return_a_read_command_if_the_input_is_read_a_user() {
+        input = "Alice";
+        expectedCommand = new Read("Alice", new DummyUserRepository(), 0);
 
         assertEquals(expectedCommand, parser.parse(input));
     }
