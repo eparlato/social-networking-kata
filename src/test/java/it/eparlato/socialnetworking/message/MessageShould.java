b5d865f46d56b3f71e72aca3cefc1cfa146bc292
eparlato <eparlato82@gmail.com>
2018-09-02 14:21:56 +0200

    Refactoring.

--- src/test/java/it/eparlato/socialnetworking/message/MessageShould.java
+++ src/test/java/it/eparlato/socialnetworking/message/MessageShould.java
@@ -1,47 +1,52 @@
 package it.eparlato.socialnetworking.message;
 
 import it.eparlato.socialnetworking.time.TweakedApplicationClock;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class MessageShould {
+    long now;
+    Message message;
+
+    @Before
+    public void setup() {
+        now = System.currentTimeMillis();
+    }
+
     @Test
     public void write_that_a_message_is_15_seconds_old_if_reading_is_required() {
-        long now = System.currentTimeMillis();
         long fifteenSecondsAgo = new TweakedApplicationClock(now).subtractSeconds(15).currentTimeMillis();
 
-        Message message = new Message("I'm in New York today! Anyone wants to have a coffee?", fifteenSecondsAgo);
+        message = new Message("I'm in New York today! Anyone wants to have a coffee?", fifteenSecondsAgo);
 
         assertEquals("I'm in New York today! Anyone wants to have a coffee? (15 seconds ago)", message.readingText(now));
     }
 
     @Test
     public void write_that_a_message_is_3_minutes_old_if_reading_required() {
-        long now = System.currentTimeMillis();
         long threeMinutesAgo = new TweakedApplicationClock(now).subtractMinutes(3).currentTimeMillis();
 
-        Message message = new Message("I want a cup of coffee", threeMinutesAgo);
+        message = new Message("I want a cup of coffee", threeMinutesAgo);
 
         assertEquals("I want a cup of coffee (3 minutes ago)", message.readingText(now));
     }
 
     @Test
     public void write_a_proper_message_if_the_message_is_one_minute_old() {
-        long now = System.currentTimeMillis();
-        long threeMinutesAgo = new TweakedApplicationClock(now).subtractMinutes(1).currentTimeMillis();
+        long oneMinuteAgo = new TweakedApplicationClock(now).subtractMinutes(1).currentTimeMillis();
 
-        Message message = new Message("I want a cup of tea", threeMinutesAgo);
+        message = new Message("I want a cup of tea", oneMinuteAgo);
 
         assertEquals("I want a cup of tea (1 minute ago)", message.readingText(now));
     }
 
     @Test
     public void write_a_proper_message_if_the_message_is_one_second_old() {
-        long now = System.currentTimeMillis();
-        long threeMinutesAgo = new TweakedApplicationClock(now).subtractSeconds(1).currentTimeMillis();
+        long oneSecondAgo = new TweakedApplicationClock(now).subtractSeconds(1).currentTimeMillis();
 
-        Message message = new Message("I feel good", threeMinutesAgo);
+        message = new Message("I feel good", oneSecondAgo);
 
         assertEquals("I feel good (1 second ago)", message.readingText(now));
     }
