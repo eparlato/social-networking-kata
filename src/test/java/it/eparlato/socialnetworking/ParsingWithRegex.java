a26024960b0bf270ee7a8c0081335eb52376fa76
eparlato <eparlato82@gmail.com>
2018-09-21 22:39:07 +0200

    Regex matches with the wall command

--- src/test/java/it/eparlato/socialnetworking/ParsingWithRegex.java
+++ src/test/java/it/eparlato/socialnetworking/ParsingWithRegex.java
@@ -8,11 +8,15 @@ import static org.junit.Assert.assertTrue;
 
 public class ParsingWithRegex {
 
-    private String regex = "\\w*";
+    private String regex = "(\\w*)((\\s(wall))*)";
 
     @Test
     public void regex_should_match_the_username_alone() {
-
         assertTrue("Username doesn't match", Pattern.matches(regex, "Alice"));
+    }
+
+    @Test
+    public void regex_should_match_the_wall_command() {
+        assertTrue("Wall command doesn't match", Pattern.matches(regex, "Bob wall"));
     }
 }
