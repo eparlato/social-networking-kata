b69f7f0f5904cd594a7d637fcf09cd0ee47af1a9
eparlato <eparlato82@gmail.com>
2018-09-22 17:57:58 +0200

    It is possible to extract the strings of a command from the regex groups.

--- src/test/java/it/eparlato/socialnetworking/ParsingWithRegex.java
+++ src/test/java/it/eparlato/socialnetworking/ParsingWithRegex.java
@@ -1,14 +1,17 @@
 package it.eparlato.socialnetworking;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ParsingWithRegex {
 
-    private String regex = "(\\w*)(\\s(wall|->|follows)(\\s\\w*)*)*";
+    private String regex = "(\\w*)(\\s(wall|->|follows)(\\s(\\w*))*)*";
 
     @Test
     public void regex_should_match_the_viewtimeline_command() {
@@ -28,6 +31,17 @@ public class ParsingWithRegex {
     @Test
     public void regex_should_match_the_follows_command() {
         assertTrue("Follows command doesn't match", matches("Bob follows Ed"));
+    }
+
+    @Test
+    public void should_be_possible_to_extract_follows_command_sintax_from_regex_groups() {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher("Bob follows Charlie");
+        matcher.find();
+
+        assertEquals("First group doesn't contain the username","Bob", matcher.group(1));
+        assertEquals("Third group doesn't contain the command", "follows", matcher.group(3));
+        assertEquals("Fifth group doesn't contain the command parameter", "Charlie", matcher.group(5));
     }
 
     private boolean matches(String input) {
